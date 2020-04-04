7594078550618271d09dd3be8cc8853f4acd27ad
Ganeshsing Midar <gmidar19@in.ibm.com>
2020-04-04 22:09:27 +0530

    Serving HTML pages

--- Sec5Assignment/routes/shop.js
+++ Sec5Assignment/routes/shop.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 
 const router = express.Router();
 
@@ -6,7 +7,10 @@ router.get("/", (req, res, next) => {
   console.log("In another middleware");
   console.log("In another middleware");
 
-  res.send("<h1>Hello from Express js</h1>");
+  // res.send("<h1>Hello from Express js</h1>");
+
+  // for configuring file path
+  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
 });
 
 module.exports = router;
