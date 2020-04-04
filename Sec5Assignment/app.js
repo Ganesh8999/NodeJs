d916314f69d93a9c1ef1be15bea1e33f412ce302
Ganeshsing Midar <gmidar19@in.ibm.com>
2020-04-04 17:11:50 +0530

    adding a 404  Error Page

--- Sec5Assignment/app.js
+++ Sec5Assignment/app.js
@@ -28,6 +28,9 @@ const bodyParser = require("body-parser");
 
 app.use(adminRoutes);
 app.use(shopRoutes);
+app.use((req, res, next) => {
+  res.status(404).send("<h1> Page Not Found !!!!! ");
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
