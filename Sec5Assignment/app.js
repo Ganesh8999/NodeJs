0ac2f52d08611c3ce521c793a8b6f3b17b382fe4
Ganeshsing Midar <gmidar19@in.ibm.com>
2020-04-06 18:55:48 +0530

    Adding config for ejs,pg and express-handlebars

--- Sec5Assignment/app.js
+++ Sec5Assignment/app.js
@@ -1,13 +1,13 @@
 const express = require("express");
-
-const path = require("path");
-
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
-
 const app = express();
 
+// const rootDir = require("../Sec5Assignment/util");
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
 const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // app.use((req, res, next) => {
 //   console.log("First Middleware !! ");
@@ -29,12 +29,11 @@ const bodyParser = require("body-parser");
 // });
 
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/admin", adminRoutes);
+//app.use("/admin", adminRoutes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, "./", "views", "404.html"));
 });
-
-app.use(bodyParser.urlencoded({ extended: false }));
 
 app.listen(3000);
--- Sec5Assignment/routes/shop.js
+++ Sec5Assignment/routes/shop.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const router = express.Router();
+const adminData = require("./admin");
 
 router.get("/", (req, res, next) => {
   console.log("In another middleware");
@@ -10,6 +11,8 @@ router.get("/", (req, res, next) => {
   // res.send("<h1>Hello from Express js</h1>");
 
   // for configuring file path
+  console.log(adminData.products);
+
   res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
 });
 
