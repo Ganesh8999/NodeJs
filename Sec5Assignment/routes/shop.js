2615204627ad2b71bc455cf4374bd386801578da
Ganeshsing Midar <gmidar19@in.ibm.com>
2020-04-07 13:57:02 +0530

    finishing the pug template

--- Sec5Assignment/routes/shop.js
+++ Sec5Assignment/routes/shop.js
@@ -1,20 +1,37 @@
-const express = require("express");
 const path = require("path");
 
-const router = express.Router();
+const express = require("express");
+
+//const rootDir = require('../util/path');
 const adminData = require("./admin");
 
+const router = express.Router();
+
 router.get("/", (req, res, next) => {
-  console.log("In another middleware");
-  console.log("In another middleware");
-
-  // res.send("<h1>Hello from Express js</h1>");
-
-  // for configuring file path
-  console.log(adminData.products);
-
-  // res.sendFile(path.join(__dirname, "../", "views", "shop.html")); // removed for implementing pug engine
-  res.render("shop.pug");
+  const products = adminData.products;
+  res.render("shop", { prods: products, pageTitle: "Shop", path: "/" });
 });
 
 module.exports = router;
+// const express = require("express");
+// const path = require("path");
+
+// const router = express.Router();
+// const adminData = require("./admin");
+
+// router.get("/", (req, res, next) => {
+//   console.log("In another middleware");
+//   console.log("In another middleware");
+
+//   // res.send("<h1>Hello from Express js</h1>");
+
+//   // for configuring file path
+//   console.log(adminData.products);
+
+//   const products = adminData.products;
+
+//   // res.sendFile(path.join(__dirname, "../", "views", "shop.html")); // removed for implementing pug engine
+//   res.render("shop.pug", { prods: products, docTitle: "Shop" });
+// });
+
+// module.exports = router;
