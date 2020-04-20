88b7493788bcd9ad9a81b5f4e59c6cc7855bda86
Ganeshsing Midar <gmidar19@in.ibm.com>
2020-04-20 21:03:25 +0530

    MongoDB config changed due to MongoDB selection er

--- ShopAppWithMongoDB/util/db.js
+++ ShopAppWithMongoDB/util/db.js
@@ -4,7 +4,7 @@ let _db;
 const mongoConnect = (callback) => {
   const MongoClient = mongodb.MongoClient;
   MongoClient.connect(
-    "mongodb+srv://nodeg:nodeg@mongoatlascluster-4ebqp.gcp.mongodb.net/shop?retryWrites=true&w=majority",
+    "mongodb://nodeg:nodeg@mongoatlascluster-shard-00-00-4ebqp.gcp.mongodb.net:27017,mongoatlascluster-shard-00-01-4ebqp.gcp.mongodb.net:27017,mongoatlascluster-shard-00-02-4ebqp.gcp.mongodb.net:27017/shop?ssl=true&replicaSet=MongoAtlasCluster-shard-0&authSource=admin&retryWrites=true&w=majority",
     { useUnifiedTopology: true }
   )
     .then((client) => {
