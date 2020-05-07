176bfe23e86f2bf31c627599f52cf7036d276b94
Ganeshsing Midar <gmidar19@in.ibm.com>
2020-05-07 13:57:25 +0530

    added deleteFile

---
+++ ShopAsyncReqDemo/public/js/admin.js
@@ -0,0 +1,6 @@
+const deleteFile = (btn) => {
+  // console.log("clicked!!!!");
+
+  const prodId = btn.parentNode.querySelector('[name="productId"]').value;
+  const csrf = btn.parentNode.querySelector('[name="_csrf"]').value;
+};
