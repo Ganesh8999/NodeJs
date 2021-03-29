84ace1e97dd2115c45dcca9ba60bb54d25389db4
Ganesh8999 <Ganesh8999@github.com>
2021-03-29 06:03:50 +0530

    use fake timers

--- Sinon-Demo/test/first.js
+++ Sinon-Demo/test/first.js
@@ -1,7 +1,6 @@
 const assert = require("assert");
-
 const greeter = require("../greeter.js");
-
+const sinon = require("sinon");
 describe("just a silly test", function () {
   it("checks a greetName", function () {
     //  assert.strictEqual(2 + 2, 3);
@@ -9,5 +8,15 @@ describe("just a silly test", function () {
       greeter.greetName("Ganesh"),
       "Hello , Ganesh Today is Monday, March 29, 2021"
     );
+  });
+
+  it("checks a greetName with fake date", function () {
+    //  assert.strictEqual(2 + 2, 3);
+    var clock = sinon.useFakeTimers(new Date(2021, 0, 15));
+    assert.strictEqual(
+      greeter.greetName("Ganesh"),
+      "Hello , Ganesh Today is Friday, January 15, 2021"
+    );
+    clock.restore();
   });
 });
