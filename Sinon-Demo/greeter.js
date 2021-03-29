45db1197741b01fc08c8c3cdcb474c049e1e7f43
Ganesh8999 <Ganesh8999@github.com>
2021-03-29 05:57:09 +0530

    exporting greetName and checking test

--- Sinon-Demo/greeter.js
+++ Sinon-Demo/greeter.js
@@ -1,19 +1,40 @@
 // const abc = greetName(process.argv[2]);
 // console.log(abc);
 
-function greetName(name) {
-  console.log("j");
+// for exporting
+(function (exports) {
+  function greetName(name) {
+    console.log("j");
 
-  var options = {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  };
+    var options = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
 
-  var now = new Date();
+    var now = new Date();
 
-  var formattedDate = now.toLocaleDateString("en-US", options);
+    var formattedDate = now.toLocaleDateString("en-US", options);
 
-  return `Hello , ${name} Today is ${formattedDate}`;
-}
+    return `Hello , ${name} Today is ${formattedDate}`;
+  }
+
+  exports.greetName = greetName;
+})(this);
+// function greetName(name) {
+//   console.log("j");
+
+//   var options = {
+//     weekday: "long",
+//     year: "numeric",
+//     month: "long",
+//     day: "numeric",
+//   };
+
+//   var now = new Date();
+
+//   var formattedDate = now.toLocaleDateString("en-US", options);
+
+//   return `Hello , ${name} Today is ${formattedDate}`;
+// }
--- Sinon-Demo/test/first.js
+++ Sinon-Demo/test/first.js
@@ -1,7 +1,13 @@
 const assert = require("assert");
 
+const greeter = require("../greeter.js");
+
 describe("just a silly test", function () {
-  it("checks a sum", function () {
-    assert.equal(2 + 2, 3);
+  it("checks a greetName", function () {
+    //  assert.strictEqual(2 + 2, 3);
+    assert.strictEqual(
+      greeter.greetName("Ganesh"),
+      "Hello , Ganesh Today is Monday, March 29, 2021"
+    );
   });
 });
